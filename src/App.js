54f66dc29f5ea18efa9bd9f075274399c68faeb2
pradeep dheke <pradeepbarca029@gmail.com>
2022-03-22 09:21:41 +1100

    added nav var in react-app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 
+import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
 import './App.css';
 
 const App = () => {
   return (
-    <div>Hello</div>
+    <div>
+      <Alert variant="primary">
+    This is an alert—check it out!
+  </Alert>
+  <Button variant= "danger" className="">
+    Danger
+  </Button>
+    </div>
   );
 }
 
